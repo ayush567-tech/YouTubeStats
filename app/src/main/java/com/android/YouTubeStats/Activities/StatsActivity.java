c1a75353f28abbc5d5917beed76792a699a30a9f
Ayush567-tech <ayushupadhy@gmail.com>
2020-10-23 21:18:09 +0530

    Initial Commit

---
+++ app/src/main/java/com/android/YouTubeStats/Activities/StatsActivity.java
@@ -0,0 +1,17 @@
+package com.android.YouTubeStats.Activities;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+import com.android.YouTubeStats.R;
+
+
+public class StatsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_stats);
+
+    }
+
+}
